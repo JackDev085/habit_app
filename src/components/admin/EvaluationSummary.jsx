50363acf73447dbeaaf5c07c861fc35288991876
jack <computadordb@gmail.com>
2026-03-10 16:09:11 -0300

    add: view muscle_in_risk in admin page

--- src/components/admin/EvaluationSummary.jsx
+++ src/components/admin/EvaluationSummary.jsx
@@ -18,6 +18,26 @@ export default function EvaluationSummary({ evaluation }) {
         </h4>
         <MetricChips obj={pos} />
       </div>
+
+      {evaluation?.muscle_in_risk && (
+        <div>
+          <h4 className="text-xs uppercase tracking-wide text-neutral-400 mb-2 ">
+            Músculos Dor Águda
+          </h4>
+          <div className="flex flex-col gap-2 w-fit">
+            {Object.entries(evaluation?.muscle_in_risk ?? {}).map(
+              ([key, value]) => (
+                <span
+                  key={key}
+                  className="px-2 py-1 text-xs rounded-sm bg-red-500 text-white"
+                >
+                  {key}:{value}
+                </span>
+              ),
+            )}
+          </div>
+        </div>
+      )}
     </div>
   );
 }
