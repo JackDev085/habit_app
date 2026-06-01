ed68fc4fab1a6af4c1ffa7009e399b62b14bd032
jack <computadordb@gmail.com>
2026-05-31 22:00:13 -0300

    mend

--- api/api.jsx
+++ api/api.jsx
@@ -1,6 +1,7 @@
 import axios from "axios";
 import { toast } from "react-toastify";
 
+
 const api = axios.create({
   // baseURL: "http://localhost:8000",
   baseURL: "https://habit-backend-five.vercel.app",
